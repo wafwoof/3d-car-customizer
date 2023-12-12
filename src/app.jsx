1d08e182d69d70c9f8a7f81d70ac5a69da9656f0
wafwoof <kazmcquaid@gmail.com>
2023-12-12 00:35:25 -0800

    tracking commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import { render } from 'preact'
 import { useState, useEffect } from 'preact/hooks'
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { injectSpeedInsights } from "@vercel/speed-insights"
 import './app.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
